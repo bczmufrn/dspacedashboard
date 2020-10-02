c724010c81d1532951e4da0535e432eb06624c91
Anderson Queiroz <andersonkeiroz@gmail.com>
2020-10-01 22:02:00 -0300

    Modifica URL de arquivos estáticos

--- dspacedashboard/settings.py
+++ dspacedashboard/settings.py
@@ -148,7 +148,7 @@ AUTH_USER_MODEL = 'accounts.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #Media
